6cc48f2619abd142d979759712cfc3ffe0c868c4
harrist03 <152608673+harrist03@users.noreply.github.com>
2024-12-02 22:58:07 +0000

    question 4 completed

---
+++ OOPCA2/Cell.java
@@ -0,0 +1,10 @@
+package OOPCA2;
+
+public class Cell {
+    public int row, column;
+
+    public Cell (int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}
--- OOPCA2/Question4.java
+++ OOPCA2/Question4.java
@@ -1,55 +1,79 @@
 package OOPCA2;
 
-import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
- *  Name:
- *  Class Group:
+ * Name: Harris Teh Kai Ze
+ * Class Group: SD2B
  */
 
-public class Question4  // Flood Fill (Stack, 2D Array)
+public class Question4 // Flood Fill (Stack, 2D Array)
 {
     public static void main(String[] args) {
         start();
     }
-    public static void start()
-    {
+
+    public static void start() {
         int[][] arr = floodFillStart();
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Enter starting row (0-9): ");
+        int startRow = kb.nextInt();
+        System.out.println("Enter starting column (0-9)");
+        int startCol = kb.nextInt();
+        fill(arr, startRow, startCol);
+        display(arr);
+
+        kb.close();
     }
 
     /*
-        Starter function to create the 2D array and populate it with zeros
+     * Starter function to create the 2D array and populate it with zeros
      */
-    public static int[][]  floodFillStart() {
-        Scanner kb = new Scanner(System.in);
+    public static int[][] floodFillStart() {
         int[][] arr = new int[10][10];
-        for (int x = 0; x < 10; x++)
-        {
-            for (int y = 0; y < 10; y++)
-            {
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
                 arr[x][y] = 0;
             }
         }
         return arr;
     }
+
+    private static void fill(int[][] arr, int startRow, int startCol) {
+        Stack<Cell> stack = new Stack<>();
+        // pushes the row and column input (e.g. 0 and 0)
+        stack.push(new Cell(startRow, startCol));
+        int fillNumber = 1;
+
+        while (!stack.isEmpty()) {
+            // pop the coordinates 0 and 0
+            Cell cell = stack.pop();
+            int row = cell.row;
+            int col = cell.column;
+
+            if (row >= 0 && row < 10 && col >= 0 && col < 10 && arr[row][col] == 0) {
+                // row 0 and col 0 should be 1
+                arr[row][col] = fillNumber++;
+                // north, south, east, west
+                stack.push(new Cell(row - 1, col));
+                stack.push(new Cell(row + 1, col));
+                stack.push(new Cell(row, col - 1));
+                stack.push(new Cell(row, col + 1));
+            }
+        }
+    }
+
     /*
-        Helper function to display the image
+     * Helper function to display the image
      */
-    public static void display(int[][] arr)
-    {
-        for (int x = 0; x < 10; x++)
-        {
-            for (int y = 0; y < 10; y++)
-            {
+    public static void display(int[][] arr) {
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
                 System.out.printf("%4d", arr[x][y]);
             }
             System.out.println();
         }
-    }
-    private static void fill(int r, int c, int[][] arr)
-    {
-
     }
 
 }
