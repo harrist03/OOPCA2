c969542097ce4cbd44c503d75cca193e7e463317
harrist03 <152608673+harrist03@users.noreply.github.com>
2024-12-05 23:13:59 +0000

    question 7 completed

---
+++ OOPCA2/Block.java
@@ -0,0 +1,11 @@
+package OOPCA2;
+
+public class Block {
+    public int qty;
+    public double price;
+
+    public Block (int qty, double price) {
+        this.qty = qty;
+        this.price = price;
+    }
+}
